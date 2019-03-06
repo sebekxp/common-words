efe1de152a6671ae88725f358ab08b0eb07eeecf
Sebastian Paszyński <p15sebek@gmail.com>
2019-03-06 23:49:11 +0100

    use dynamic data/ prepare for server

---
+++ objectWord.js
@@ -0,0 +1,31 @@
+/*  
+        <div id="a3" class="word-content">
+            <h3>able</h3>
+            <p>zdolny (do czegoś)</p>
+        </div>*/
+const objWords = [
+    {
+        wordName: 'a',
+        wordContent: ["Przedimek nieokreślony używany z rzeczownikami policzalnymi w liczbie pojedynczej, których wymowa zaczyna się od spółgłoski",
+            "Is there a problem? - Czy jest jakiś problem?",
+            "You have a job. - Ty masz pracę.",
+            "He needs a dollar. - On potrzebuje dolara."
+        ]
+    },
+
+    {
+        wordName: 'ability',
+        wordContent: ["umiejętność, zdolność",
+            "You have an ability to problem-solve. - Masz zdolność do rozwiązywania problemów.",
+        ]
+    },
+
+    {
+        wordName: 'able',
+        wordContent: ["zdolny (do czegoś)",
+        ]
+    },
+
+
+
+];
--- scrp.js
+++ scrp.js
@@ -1,6 +1,136 @@
 let knownWords = 0;
 let valueofLastItem = getValueOfLastItem();
 let updatedValueOfProgressBar = 0;
+const words = objWords;
+
+document.querySelector('#complete').innerHTML = knownWords + ' / ' + valueofLastItem;
+
+
+function openWord(evt) {
+    console.log(evt.currentTarget)
+
+    let wordContentAllParagraph = document.querySelectorAll('.word-content p');
+    let wordContentAllH2 = document.querySelectorAll('.word-content h2');
+    for (let i of wordContentAllParagraph) {
+        i.innerHTML = "";
+    }
+    for (let i of wordContentAllH2) {
+        i.innerHTML = "";
+    }
+    
+
+    wordContent.innerHTML = "";
+    // for (let i = 0; i < wordContent.length; i++) {
+    //     wordContent[i].style.display = "none";
+    // }
+    // document.getElementById(wordName).style.display = "block";
+}
+function searchWord() {
+    let valeFromSearchBox = document.getElementById("search-box").value.toUpperCase();;
+    let listOfWords = document.getElementsByClassName("list-of-words")[0];
+    let wordsContent = listOfWords.getElementsByClassName("words");
+    let wordTitle = document.getElementsByClassName('word-title');
+
+    for (let i = 0; i < wordsContent.length; i++) {
+        if (wordsContent[i].innerHTML.toUpperCase().indexOf(valeFromSearchBox) > -1) {
+            wordTitle[i].style.display = "";
+        } else {
+            wordTitle[i].style.display = "none";
+        }
+    }
+}
+
+function updateProgressBar() {
+    knownWords = document.querySelectorAll('.filled-circle');
+    updatedValueOfProgressBar = parseInt(knownWords.length * (100 / valueofLastItem), 10);
+    document.querySelector('#complete').innerHTML = knownWords.length + ' / ' + valueofLastItem;
+    document.querySelector('.progBar').style.width = updatedValueOfProgressBar + '%';
+}
+
+function progresBar(evt) {
+    if (evt.target.classList[1] === "far") {
+        evt.target.className = evt.target.classList[0] + " filled-circle fas fa-check-circle";
+    }
+    else {
+        evt.target.className = evt.target.classList[0] + " blank-circle far fa-circle";
+    }
+
+    updateProgressBar();
+}
+function useEnterToCreateNewWord() {
+    if (event.which == 13 || event.keyCode == 13) {
+        createNavigationWord();
+    }
+}
+function createContentOfWord({wordName, wordContent}) {
+
+    let wordContentContainer = document.querySelector('.word-content');
+
+    let nameWord = document.createElement("h2")
+    nameWord.innerHTML = wordName;
+    wordContentContainer.appendChild(nameWord);
+
+    let contents = [wordContent.length]
+    for (it of wordContent) {
+        contents = document.createElement('p');
+        contents.innerHTML = it;
+        wordContentContainer.appendChild(contents);
+    }
+}
+function createNavigationWord(wordObj) {
+    let wordTitleDiv = document.createElement("div");
+    let circleIconDiv = document.createElement("div");
+    let blankCircleDiv = document.createElement("p");
+    let wordNameDiv = document.createElement("p");
+    let deleteElemI = document.createElement("i");
+
+    wordTitleDiv.className = "word-title";
+    circleIconDiv.className = "circle-icon";
+    blankCircleDiv.className = "blank-circle far fa-circle";
+    wordNameDiv.className = "words";
+    deleteElemI.className = "deleteElem fas fa-times";
+
+    blankCircleDiv.addEventListener('click', progresBar);
+    circleIconDiv.addEventListener('click', deleteNavigationWord);
+
+    wordNameDiv.innerHTML = wordObj.wordName;
+
+    //                         IF USER ADD 
+    // ===========================================================================||
+    // ||if (document.querySelector('.input-name').value === "")                  ||
+    // ||    wordNameDiv.innerHTML = "Default name"                               ||
+    // ||else                                                                     ||
+    // ||   wordNameDiv.innerHTML = document.querySelector('.input-name').value   ||
+    // ||wordNameDiv.setAttribute('contenteditable', "true");                     |
+        // wordNameDiv.setAttribute('title', "Click to change the name");|
+    // =============================================================================
+
+
+    wordTitleDiv.addEventListener("mouseover", openWord);
+    circleIconDiv.appendChild(deleteElemI);
+    wordTitleDiv.appendChild(circleIconDiv);
+    wordTitleDiv.appendChild(blankCircleDiv);
+    wordTitleDiv.appendChild(wordNameDiv);
+
+    createContentOfWord(wordObj); 
+
+    document.querySelector('.list-of-words').appendChild(wordTitleDiv);
+    document.querySelector('.input-name').value = "";
+
+    valueofLastItem = getValueOfLastItem();
+    updateProgressBar();
+
+}
+
+function deleteNavigationWord(evt) {
+    evt.currentTarget.parentElement.remove();
+    valueofLastItem = getValueOfLastItem();
+    updateProgressBar();
+}
+
+for (let word of words) {
+    createNavigationWord(word);
+}
 
 
 let allBlankCircle = document.querySelectorAll('.blank-circle')
@@ -19,112 +149,4 @@ for (var i = 0; i < allCircleIcon.length; i++) {
 
 function getValueOfLastItem() {
     return document.querySelectorAll('.word-title').length;
-}
-
-function openWord(wordName) {
-
-    let wordContent = document.getElementsByClassName("word-content");
-    for (var i = 0; i < wordContent.length; i++) {
-        wordContent[i].style.display = "none";
-    }
-    document.getElementById(wordName).style.display = "block";
-}
-function searchWord() {
-    let valeFromSearchBox = document.getElementById("search-box").value.toUpperCase();;
-    let listOfWords = document.getElementsByClassName("list-of-words")[0];
-    let wordsContent = listOfWords.getElementsByClassName("words");
-    let wordTitle = document.getElementsByClassName('word-title');
-
-    for (var i = 0; i < wordsContent.length; i++) {
-        if (wordsContent[i].innerHTML.toUpperCase().indexOf(valeFromSearchBox) > -1) {
-            wordTitle[i].style.display = "";
-        } else {
-            wordTitle[i].style.display = "none";
-        }
-    }
-}
-
-function updateProgresBar() {
-    updatedValueOfProgressBar = parseInt(knownWords * (100 / valueofLastItem), 10);
-    document.querySelector('#complete').innerHTML = updatedValueOfProgressBar + ' %';
-    document.querySelector('.progBar').style.width = updatedValueOfProgressBar + '%';
-}
-
-function progresBar(evt) {
-    if (knownWords <= valueofLastItem && evt.target.classList[2] === "far") {
-        knownWords++;
-    }
-    else {
-        knownWords--;
-    }
-    updateProgresBar();
-    if (evt.target.classList[2] === "far") {
-        evt.target.className = evt.target.classList[0] + " filled-circle fas fa-check-circle";
-    }
-    else {
-        evt.target.className = evt.target.classList[0] + " blank-circle far fa-circle";
-    }
-}
-function useEnterToCreateNewWord() {
-    if (event.which == 13 || event.keyCode == 13) {
-        createNavigationWord();
-    }
-}
-function createContentOfWord() {
-    var contentOfWord = document.createElement("div");
-    var contentOfWord_H2 = document.createElement("h2")
-    var contentOfWord_p = document.createElement("p");
-
-    contentOfWord.id = "a" + (valueofLastItem + 1);
-    contentOfWord.className = "word-content";
-    contentOfWord_H2.innerHTML = "a" + (valueofLastItem + 1);
-    contentOfWord_p.innerHTML = "SSSSS" + + (valueofLastItem + 1);
-
-    contentOfWord.appendChild(contentOfWord_H2);
-    contentOfWord.appendChild(contentOfWord_p);
-
-    document.querySelector('section').appendChild(contentOfWord);
-}
-function createNavigationWord() {
-    updateProgresBar();
-    var wordTitleDiv = document.createElement("div");
-    var circleIconDiv = document.createElement("div");
-    var pFirstElem = document.createElement("p");
-    var pSecondElem = document.createElement("p");
-    var deleteElemI = document.createElement("i");
-
-    createContentOfWord();
-
-    deleteElemI.className = "deleteElem fas fa-times";
-    circleIconDiv.className = "circle-icon";
-    wordTitleDiv.className = "word-title";
-    pFirstElem.className = "w" + (valueofLastItem + 1) + " blank-circle far fa-circle"
-    pSecondElem.className = "words";
-
-    pFirstElem.addEventListener('click', progresBar);
-    circleIconDiv.addEventListener('click', deleteNavigationWord);
-
-    if (document.querySelector('.input-name').value === "")
-        pSecondElem.innerHTML = "Default name"
-    else
-        pSecondElem.innerHTML = document.querySelector('.input-name').value
-
-    pSecondElem.setAttribute('contenteditable', "true");
-    pSecondElem.setAttribute('title', "Click to change the name");
-    wordTitleDiv.setAttribute("onmouseover", "openWord('a" + (valueofLastItem + 1) + "')");
-    circleIconDiv.appendChild(deleteElemI);
-    wordTitleDiv.appendChild(circleIconDiv);
-    wordTitleDiv.appendChild(pFirstElem);
-    wordTitleDiv.appendChild(pSecondElem);
-
-    document.querySelector('.list-of-words').appendChild(wordTitleDiv);
-    document.querySelector('.input-name').value = "";
-    valueofLastItem = getValueOfLastItem();
-}
-
-function deleteNavigationWord(evt) {
-    evt.currentTarget.parentElement.remove();
-    // knownWords--;
-    updateProgresBar();
-    valueofLastItem = getValueOfLastItem();
 }
